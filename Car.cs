5d0dd256d5b68600bbddbc5f082197b39ff3c8b2
Casanova <dragonthing0321@gmail.com>
2026-04-14 08:08:55 -0500

    Update Car.cs

--- Car.cs
+++ Car.cs
@@ -8,11 +8,11 @@ namespace Properties_Drake_Boswell
 {
     internal class Car
     {
-
+        // This is a backing field for the Model property.
         private string model;
-
+        // This is a property with a backing field. It allows us to control how the model is accessed and modified.
         public string Model { get { return model; } set { model = value; } }
-
+        // This is an auto-implemented property. It does not require a backing field.
         public string Make { get; set; }
     }
 }
